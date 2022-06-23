37fe95ba8166fe91038e74dde58e5397a5408d4e
Nsa Kingsley <nsakingsley007.kn@gmail.com>
2022-06-24 00:08:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let fullName = "Nsa Kingsley";
+let height = "5ft 10inches";
+let country = "Nigeria";
+
+console.log(fullName);
+console.log(height);
+console.log(country);
+
+
+document.write(fullName + "<br>" + height + "<br>" + country);
